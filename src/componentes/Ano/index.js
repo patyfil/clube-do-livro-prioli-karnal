5ac82c07e2eda8f8afdb12ff869244bce67ba2c7
patyfil <patyfil@gmail.com>
2023-05-23 12:36:49 -0300

    alt css e mobile

--- src/componentes/Ano/index.js
+++ src/componentes/Ano/index.js
@@ -2,22 +2,26 @@ import Livro from '../Livro'
 import './Ano.css'
 
 const Ano = (props) => {
-    const css = { backgroundColor: props.corSecundaria }
+    const css = { backgroundColor: props.corSecundaria };
+    const h3Style = {
+        '--cor-primaria': props.corPrimaria, // Define a variável CSS --cor-primaria com a cor primária
+    };
     return (
-        // props.livros.length > 0 &&
+
         (props.livros.length > 0) ?
-        <section className="ano" style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <div className='livros'>
-                {props.livros.map(livro => <Livro corDeFundo={props.corPrimaria} key={livro.nome}
-                    nome={livro.nome}
-                    autor={livro.autor}
-                    imagem={livro.imagem}
-                />)}
-            </div>
-        </section >
-        : ''
-    )
-}
+            <section className="ano" style={css}>
+                <h3 style={h3Style}>{props.nome}</h3>
+                <div className='livros'>
+                    {props.livros.map(livro => <Livro corDeFundo={props.corPrimaria} key={livro.nome}
+                        nome={livro.nome}
+                        autor={livro.autor}
+                        imagem={livro.imagem}
+                    />)}
+                </div>
+            </section>
+            : ''
+    );
+};
+
 
 export default Ano
