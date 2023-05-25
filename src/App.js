df4a0fdfa3c684f89e698210d7fbfc286525acff
patyfil <patyfil@gmail.com>
2023-05-25 01:51:43 -0300

    add livros

--- src/App.js
+++ src/App.js
@@ -139,6 +139,114 @@ function App() {
       imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/3-2022/08.jpg?raw=true",
       ano: edicao[1].nome
     },
+    {
+      titulo: "O Anel de Giges",
+      autor: "Eduardo Giannetti",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/3-2022/09.jpg?raw=true",
+      ano: edicao[1].nome
+    },
+    {
+      titulo: "Humanidade",
+      autor: "Rutger Bregman",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/01.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "Eu sou dinamite!",
+      autor: "Sue Prideaux",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/02.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "Fascismo à brasileira",
+      autor: "Pedro Doria",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/03.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "As grandes estratégias",
+      autor: "John Lewis Gaddis",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/04.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "Novas formas de amar",
+      autor: "Regina Navarro Lins ",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/05.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "Internacionalismo ou extinção",
+      autor: "Noam Chomsky",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/06.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "A metamorfose",
+      autor: "Franz Kafka",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/07.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "O dilema do porco-espinho",
+      autor: "Leandro Karnal",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/2-2021/08.jpg?raw=true",
+      ano: edicao[2].nome
+    },
+    {
+      titulo: "História da Riqueza do Homem - Do Feudalismo ao Século XXI",
+      autor: "Leo Huberman",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/01.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "Justiça: O que é fazer a coisa certa",
+      autor: "Michael J. Sandel",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/02.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "Conservadorismo: Um convite a grande tradição",
+      autor: "Roger Scruton",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/03.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "O chamado da tribo: Grandes pensadores para o nosso tempo",
+      autor: "Mario Vargas Llosa",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/04.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "Caminhos da esquerda - Elementos para uma reconstrução",
+      autor: "Ruy Fausto",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/05.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "Sobre o autoritarismo brasileiro",
+      autor: "Lilia M. Schwarcz",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/06.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "O valor das ideias: Debate em tempos turbulentos",
+      autor: "Marcos Lisboa e Samuel Pessôa",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/07.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "O povo contra a democracia: Por que nossa liberdade corre perigo e como salvá-la",
+      autor: "Yascha Mounk",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/08.jpg?raw=true",
+      ano: edicao[3].nome
+    },
+    {
+      titulo: "A revolução dos bichos: Um conto de fadas",
+      autor: "George Orwell",
+      imagem: "https://github.com/patyfil/clube-do-livro-prioli-karnal/blob/main/public/imagens/1-2020/09.jpg?raw=true",
+      ano: edicao[3].nome
+    },
   ]
   const [livros, setLivros] = useState(inicial)
 
